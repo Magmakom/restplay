18e01fcef3522e62bbd8522f635fde2fa73cfc42
John <vammp2008@gmail.com>
2015-12-07 05:35:46 +0200

    add json transformer

--- app/controllers/RestaurantController.scala
+++ app/controllers/RestaurantController.scala
@@ -4,15 +4,23 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json._
 
 import repository.RestaurantRepository
 
 class RestaurantController extends Controller {
 
+  val jsonTransformer = (__).json.update( (__ \ '_id).json.copyFrom( (__ \ '_id \ '$oid).json.pick ) ) //'
+
   def findAllAsJson = Action.async { request =>
     RestaurantRepository.findAll().map { result =>
-        Ok(Json.toJson(result))
+      Json.toJson(result) match {
+        case JsArray(elements) => {
+          val out = elements.map(_.transform(jsonTransformer).get)
+          Ok(Json.toJson(out))
+        }
+        case _ => sys.error("Oops! JsArray expected.")
+      }
     }
   }
 
