9d04cd0b815233929f66ca62f06dcc5054e2cb51
Magmakom <magmakomer@gmail.com>
2015-12-22 21:06:58 +0200

    -Added Create/Edit/Delete buttons for restaurants

--- public/js/main.js
+++ public/js/main.js
@@ -111,6 +111,7 @@ function putMarker(obj) {
 
 //generate content for marker popup
 function getContent(obj) {
+    restHref = "/api/restaurant/"+obj["_id"];
     result =
         '<b>' + obj["name"] + '</b>' +
         '<br>' + obj["description"] + '<br><br>' +
@@ -149,6 +150,12 @@ function openRestInfoPage() {
     $('#info_container').css({"visibility": "visible", "display": "block"});
     $('#mapButton').removeAttr("active");
     marker.closePopup();
+    setUrl(restaurantList.get(curRestIndex).name, "/api/review/by-rest/"+restaurantList.get(curRestIndex)._id);
+   // slider('reviewSlider', 0);
+}
+
+function setUrl(title, newUrl){
+    window.history.pushState("string", title, newUrl);
 }
 
 function editRestaurant(_id){
@@ -236,6 +243,96 @@ function updateRatingPage(){
 function setAutorized(){
     autorized=true;
 }
+
+
+
+
+
+
+var arrayReviews = new Array();
+var reviewSpeedOpen = 10;
+var reviewTimer = 10;
+
+// Loop through all the divs in the slider parent div //
+// Calculate seach content divs height and set it to a variable //
+function slider(target,showfirst) {
+    var slider = document.getElementById(target);
+    var divs = slider.getElementsByTagName('div');
+    var divslength = divs.length;
+    for(i = 0; i < divslength; i++) {
+        var div = divs[i];
+        var divid = div.id;
+        if(divid.indexOf("reviewHeader") != -1) {
+            div.onclick = new Function("processClick(this)");
+        } else if(divid.indexOf("reviewContent") != -1) {
+            var section = divid.replace('-reviewContent','');
+            arrayReviews.push(section);
+            div.maxh = div.offsetHeight;
+            if(showfirst == 1 && i == 1) {
+                div.style.display = 'block';
+            } else {
+                div.style.display = 'none';
+            }
+        }
+    }
+}
+
+// Process the click - expand the selected content and collapse the others //
+function processClick(div) {
+    var catlength = arrayReviews.length;
+    for(i = 0; i < catlength; i++) {
+        var section = arrayReviews[i];
+        var head = document.getElementById(section + '-reviewHeader');
+        var cont = section + '-reviewContent';
+        var contdiv = document.getElementById(cont);
+        clearInterval(contdiv.timer);
+        if(head == div && contdiv.style.display == 'none') {
+            contdiv.style.height = '0px';
+            contdiv.style.display = 'block';
+            initSlide(cont,1);
+        } else if(contdiv.style.display == 'block') {
+            initSlide(cont,-1);
+        }
+    }
+}
+
+// Setup the variables and call the slide function //
+function initSlide(id,dir) {
+    var cont = document.getElementById(id);
+    var maxh = cont.maxh;
+    cont.direction = dir;
+    cont.timer = setInterval("slide('" + id + "')", reviewTimer);
+}
+
+// Collapse or expand the div by incrementally changing the divs height and opacity //
+function slide(id) {
+    var cont = document.getElementById(id);
+    var maxh = cont.maxh;
+    var currheight = cont.offsetHeight;
+    var dist;
+    if(cont.direction == 1) {
+        dist = (Math.round((maxh - currheight) / reviewSpeedOpen));
+    } else {
+        dist = (Math.round(currheight / reviewSpeedOpen));
+    }
+    if(dist <= 1) {
+        dist = 1;
+    }
+    cont.style.height = currheight + (dist * cont.direction) + 'px';
+    cont.style.opacity = currheight / cont.maxh;
+    cont.style.filter = 'alpha(opacity=' + (currheight * 100 / cont.maxh) + ')';
+    if(currheight < 2 && cont.direction != 1) {
+        cont.style.display = 'none';
+        clearInterval(cont.timer);
+    } else if(currheight > (maxh - 2) && cont.direction == 1) {
+        clearInterval(cont.timer);
+    }
+}
+
+
+
+
+
 
 // no needs today coz of react.js
 function updateList() {
